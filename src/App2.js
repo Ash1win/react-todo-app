96743a0f9a1c19855a81bb41fe917a7c449c3c40
Ash1win <naikashwin30@gmail.com>
2022-08-29 15:21:26 +0530

    first commit

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import App3 from "./App3"
+
+function App2() {
+    return (
+        <div>
+            <h2>Page two</h2>
+            <App3 />
+            <p>another component!</p>
+        </div>
+    )
+}
+
+export default App2
