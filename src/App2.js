6573c704c9435c73ff38f8b6651158e49cb8f81d
Ash1win <naikashwin30@gmail.com>
2022-09-01 09:29:44 +0530

    created template for mobile plan

--- src/App2.js
+++ src/App2.js
@@ -3,11 +3,21 @@ import App3 from "./App3"
 function App2() {
     return (
         <div>
-            <h2>Page two</h2>
-            <App3 />
-            <p>another component!</p>
+            <nav class="bg-slate-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+                <div class="container flex flex-wrap justify-between items-center mx-auto">
+                    <a href="#" class="flex items-center">
+                        <span class="self-center text-xl font-semibold whitespace-nowrap">Mobile Plan</span>
+                    </a>
+                </div>
+            </nav>
+            <div className="w-100 h-screen flex bg-violet-400" >
+                <div className="w-1/4 h-full text-sm font-medium py-10 px-4 bg-white">
+                </div>
+                <div className="w-3/4 h-full pt-10 flex flex-col flex-wrap content-center bg-slate-400">
+                </div>
+            </div>
         </div>
-    )
+      );
 }
 
 export default App2
---
+++ src/Table.js
@@ -0,0 +1,11 @@
+
+
+function Table() {
+    return (
+        <div>
+            <h1>table component!</h1>
+        </div>
+    )
+}
+
+export default Table
